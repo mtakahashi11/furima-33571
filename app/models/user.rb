700edcee8530d1fe3e4fa01f7cc8f5f206874241
mtakahashi11 <76547704+mtakahashi11@users.noreply.github.com>
2021-01-21 14:15:51 +0900

    Userモデル設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname,            presence: true
+         validates :encrypted_password,  format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+         validates :first_name,          presence: true
+         validates :last_name,           presence: true
+         validates :first_name_katakana, presence: true
+         validates :last_name_katakana,  presence: true
+         validates :date_of_birth,       presence: true
+   
+         has_many :items
+         has_many :purchasers
+end
