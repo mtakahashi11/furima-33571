4fe74610330caedf5c432942cbcb9735b325edd4
mtakahashi11 <76547704+mtakahashi11@users.noreply.github.com>
2021-01-29 16:17:03 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
       validates :nickname
-      validates :password,            format: {with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+      validates :password, /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
     
     with_options format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
       validates :first_name
